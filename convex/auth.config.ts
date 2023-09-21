8c1765377b0ca4beda190cdd35a409f8b8d05f9d
Matías <matias.fuentes@bicevida.cl>
2023-09-20 21:16:26 -0300

    feat: added clerk-convex auth config

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://bursting-mutt-92.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,16 @@ import ConvexClientProvider from "@/providers/ConvexClientProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConvexClientProvider>
-      <Toaster position="top-left" reverseOrder={false} />
-      <SwitchTheme />
-      <Component {...pageProps} />
+      <ClerkProvider>
+        <Toaster position="top-left" reverseOrder={false} />
+        <SwitchTheme />
+        <Component {...pageProps} />
+      </ClerkProvider>
     </ConvexClientProvider>
   );
 }
