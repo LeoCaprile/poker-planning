4041ab70c34b5fd617bf2b00351406f3c6086e81
Matias Fuentes <matiasf.cortez@gmail.com>
2023-11-12 21:25:16 -0300

    feat: added version in top right of the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import ConvexClientProvider from "@/providers/ConvexClientProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { version } from "@/../package.json";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Toaster position="top-left" reverseOrder={false} />
       <SwitchTheme />
       <Component {...pageProps} />
+      <span className="absolute top-0 right-0">version {version} </span>
     </ConvexClientProvider>
   );
 }
