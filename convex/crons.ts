216a9de7817dae0aadf96334c9940a174da1568b
Matías Fuentes <matias.fuentes@bicevida.cl>
2023-11-24 14:40:16 -0300

    fixed cron month day limit

--- convex/crons.ts
+++ convex/crons.ts
@@ -4,7 +4,7 @@ import { api } from "./_generated/api";
 const crons = cronJobs();
 crons.monthly(
   "delete rooms which are one month old",
-  { day: 0, hourUTC: 0, minuteUTC: 0 },
+  { day: 1, hourUTC: 0, minuteUTC: 0 },
   api.rooms.deleteIdleRooms
 );
 export default crons;
