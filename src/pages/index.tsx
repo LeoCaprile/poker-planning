7d898f624b3786418ebfceae7b598833945b3869
Matias Fuentes <matiasf.cortez@gmail.com>
2024-09-22 13:50:01 -0300

    feat: added logo and changed home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import CreateRoomButton from "@/modules/Room/CreateRoomButton";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center">
-      <h1 className="text-6xl text-center">Poker planing</h1>
+    <main className="flex min-h-[90vh] flex-col justify-center items-center">
+      <Image src="/cat.png" quality={100} alt="logo" width={300} height={300} />
+      <h1 className="text-6xl font-bold text-center">Poker <br /> Planning</h1>
       <CreateRoomButton />
     </main>
   );
